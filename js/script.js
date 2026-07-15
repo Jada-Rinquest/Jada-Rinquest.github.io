c6f602b9861dd046cb8106b5a2bc0e1b7b2a8204
Jada Rinquest <222871296@mycput.ac.za>
2026-07-15 13:31:47 +0200

    Add typing and erasing effect for roles display
    Implement typing effect for displaying roles in the typing element.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
+const typingElement = document.getElementById("typing");
 
+if (typingElement) {
+
+    const roles = [
+        "Aspiring Software Developer",
+        "IT Student",
+        "Future AI Professional",
+        "Problem Solver"
+    ];
+
+    let roleIndex = 0;
+    let charIndex = 0;
+
+    function type() {
+
+        if (charIndex < roles[roleIndex].length) {
+
+            typingElement.textContent += roles[roleIndex].charAt(charIndex);
+            charIndex++;
+
+            setTimeout(type, 100);
+
+        } else {
+
+            setTimeout(erase, 1500);
+
+        }
+    }
+
+    function erase() {
+
+        if (typingElement.textContent.length > 0) {
+
+            typingElement.textContent =
+                typingElement.textContent.slice(0, -1);
+
+            setTimeout(erase, 50);
+
+        } else {
+
+            roleIndex = (roleIndex + 1) % roles.length;
+            charIndex = 0;
+
+            setTimeout(type, 300);
+
+        }
+    }
+
+    typingElement.textContent = "";
+    type();
+}
